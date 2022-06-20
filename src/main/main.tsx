e363f9ae3263edd4b596220c96573782e70ad032
DavyMassoneto <davy.massoneto@yahoo.com>
2022-06-20 00:26:19 -0300

    refactor: rename app.tsx to main.tsx

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function App () {
+const Main: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -10,6 +10,8 @@ export default function App () {
     </View>
   )
 }
+
+export default Main
 
 const styles = StyleSheet.create({
   container: {
